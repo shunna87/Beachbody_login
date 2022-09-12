646ac214d3afba4b9d1bd9b4a2c7f0bae1595c49
shunna87 <anush_av@instigatemobile.com>
2022-09-12 18:11:04 +0400

    editing

--- pageobjects/LoginPage.js
+++ pageobjects/LoginPage.js
@@ -7,45 +7,41 @@ class LoginPage {
 constructor(page)
 {
     this.page = page;
-    
     this.userName = page.locator("#capture_signIn_signInEmailAddress");
     this.password = page.locator("#capture_signIn_currentPassword");
     this.loginBtn = page.locator("div[class*='topNavAccountStyled__MenuOptions'] div a");
-    this.signInBtn = page.locator("sign-in-button");
+    this.signInBtn = page.locator("form[class*='capture_form capture_signInForm'] div[class*='form-action-buttons'] button[class*='sign-in-button']");
     this.enter = page.keyboard.press('Enter');
     this.userProfile = page.locator("button[class*='userAvatarStyled__AvatarButton']");
-    
     this.logOutBtn =  page.locator("span[class*='desktopNavStyled__ItemText-sc-1lvzjju-3 desktopNavStyled__ItemLinkText-sc-1lvzjju-4 kIUfe duoYWU']");
   
 
 }
 
-async goTo()
-{
-    await this.page.goto("https://stage.beachbodyondemand.com/groups-about?locale=en_US");
-    
-    await this.loginBtn.click();
-
-            
-    
-}
 
 async validLogin(username, password)
-{
-    await this.page.userName.fill(username);
-    await this.page.userPass.fill(password);
-    await this.enter;
-     
+{  
+    // Visit home page
+    await this.page.goto(dataSet.homePage);
+    // Visit login page
+    await this.loginBtn.click();
+    // log in with the given account
+    await this.userName.fill(username);
+    await this.password.fill(password);
+    await this.signInBtn.click();
+    // Verify user profile icon is visible
+    await this.userProfile.waitFor({ state: 'visible', timeout: 20000});
+    // Verify user can logout 
+    await this.userProfile.click();
+    await this.logOutBtn.click();
+    await this.loginBtn.waitFor({ state: 'visible', timeout: 20000});
+
+       
      
 }
 
-async logOut()
-{
-    await this.page.userProfile.click();
-    await this.page.logOut.click();
-     
-               
-}
+
+
 
 }
 
--- tests/testbeachbody2.spec.js
+++ tests/testbeachbody2.spec.js
@@ -9,40 +9,14 @@ test('@Case: Verify all type of users can login (free user, couch user, club use
 
 
    const loginPage = new LoginPage(page);
-   // Visit Login page
-   await loginPage.goTo();
-   // Verify free user can login
-   await loginPage.userName.type(dataSet.email1);
-   await loginPage.password.type(dataSet.password);
-   await page.keyboard.press('Enter');
-   await loginPage.userProfile.waitFor({state: "visible", timeout: 20000});
-   // Verify free user can log out
-   await loginPage.userProfile.click();
-   await loginPage.logOutBtn.click();
-   await loginPage.loginBtn.waitFor({state: "visible", timeout: 200000});
-   // Verify coach user can login
-   await loginPage.loginBtn.click();
-   await loginPage.userName.type(dataSet.email2);
-   await loginPage.password.type(dataSet.password);
-   await page.keyboard.press('Enter');
-   await loginPage.userProfile.waitFor({state: "visible", timeout: 20000});
-   // Verify coach user can log out
-   await loginPage.userProfile.click();
-   await loginPage.logOutBtn.click();
-   await loginPage.loginBtn.waitFor({state: "visible", timeout: 200000});
-   // Verify club user can login
-   await loginPage.loginBtn.click();
-   await loginPage.userName.type(dataSet.email3);
-   await loginPage.password.type(dataSet.password);
-   await page.keyboard.press('Enter');
-   await loginPage.userProfile.waitFor({state: "visible", timeout: 20000});
-   // Verify club user can log out
-   await loginPage.userProfile.click();
-   await loginPage.logOutBtn.click();
-   await loginPage.loginBtn.waitFor({state: "visible", timeout: 200000});
+   // Verify free user can login and log out
+   await loginPage.validLogin(dataSet.email_free, dataSet.password);
+   // Verify coach user can log in and log out
+   await loginPage.validLogin(dataSet.email_coach, dataSet.password);
+   // Verify club user can login and log out
+   await loginPage.validLogin(dataSet.email_club, dataSet.password);
    
- 
-     
+        
    
 });
 
