d41def8fa8d6a48912ae288e5471c1e1fe8706c3
shunna87 <anush_av@instigatemobile.com>
2022-09-12 12:48:08 +0400

    delated readme

--- playwright.config.js
+++ playwright.config.js
@@ -1,11 +1,7 @@
 // @ts-check
 const { devices } = require('@playwright/test');
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
+
 
 
 /**
